88dfd9cb145747284e26b21a93e428051bbfa10c
Jerome <jrm.ronquillo@gmail.com>
2020-01-25 16:09:15 -0800

    first commit

---
+++ front-page.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+  * The front-page template file
+
+
+*/
+
+get_header()
+?>
+<noscript>You need to enable JavaScript to run this app.</noscript>
+<main id="site-content" role="main">
+
+
+	<div id="root">
+	</div>
+</main>
+
+
+<script src="/wp-content/themes/twentytwenty-child/static/js/2.71f23cb4.chunk.js"></script>
+<script src="/wp-content/themes/twentytwenty-child/static/js/main.9015ec5b.chunk.js"></script>
+<script src="/wp-content/themes/twentytwenty-child/static/js/runtime-main.107bb8ca.js"></script>
+
+
+
+<?php
+ get_footer();
---
+++ template-parts/react-page.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Displays the menus and widgets at the end of the main element.
+ * Visually, this output is presented as part of the footer element.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0.0
+ */
+
+
+?>
+<div>
+	<span> react test</span>
+	<div id="react_testt"></div>
+</div>
